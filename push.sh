bdc48df4b90d0111a0092af71312ca9009b83884
Emmanuel Blot <emmanuel.blot@sifive.com>
2021-06-02 17:29:05 +0200

    llvm: update the docker hub push list

--- push.sh
+++ push.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 docker login
-docker push sifive/xc3sprog:a3.13-r795        
-docker push sifive/openocd-riscv:a3.13-gd52e4668a  
-docker push sifive/clang-riscv64_dbg:a3.13-v11.0.1-n4.1
-docker push sifive/clang-riscv32_dbg:a3.13-v11.0.1-n4.1
-docker push sifive/clang-riscv64:a3.13-v11.0.1-n4.1
-docker push sifive/clang-riscv32:a3.13-v11.0.1-n4.1
-docker push sifive/gdb-riscv:a3.13-v10.1       
-docker push sifive/binutils-riscv:a3.13-v2.35.1     
-docker push sifive/clang-riscv:a3.13-v11.0.1        
+docker push sifive/clang-riscv64_dbg:a3.13-r202104-n4.1
+docker push sifive/clang-riscv32_dbg:a3.13-r202104-n4.1
+docker push sifive/clang-riscv32:a3.13-r202104-n4.1
+docker push sifive/clang-riscv64:a3.13-r202104-n4.1
+docker push sifive/clang-riscv:a3.13-r202104
+docker push sifive/llvm-riscv-nano:a3.13-r202104
+docker push sifive/qemu-fdt:d1b72f48
+docker push sifive/binutils-riscv:a3.13-v2.35-zb0.93
+docker push sifive/gcc-riscv:a3.13-v10.2.0-si5
+docker push sifive/freedom-dev:a3.13-v1.1
+docker push sifive/freedom-test:a3.13-v1.3
+docker push sifive/openfpgaloader:a3.13-ad21a3b
+docker push sifive/xc3sprog:a3.13-r795
+docker push sifive/openocd-riscv:a3.13-gd52e4668a
