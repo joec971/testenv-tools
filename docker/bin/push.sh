cea53bed8368fc85530ad11ac54bfb89627b176b
Emmanuel Blot <emmanuel.blot@sifive.com>
2021-06-25 12:06:43 +0200

    docker: add missing GDB image to push

--- docker/bin/push.sh
+++ docker/bin/push.sh
@@ -9,6 +9,7 @@ docker push sifive/llvm-riscv-nano:a3.13-r2021.06.1
 docker push sifive/qemu-fdt:d1b72f48
 docker push sifive/binutils-riscv:a3.13-r2021.06.1
 docker push sifive/gcc-riscv:a3.13-r2021.06.1
+docker push sifive/gdb-riscv:a3.13-r2021.06.1
 docker push sifive/freedom-dev:a3.13-v1.1
 docker push sifive/freedom-test:a3.13-v1.3
 docker push sifive/openfpgaloader:a3.13-ad21a3b
